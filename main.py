b9e88655eda06c2856927fd5099392945adf37ee
gdharani1996 <gdharanidharan07@gmail.com>
2023-03-11 21:18:25 +0530

    main

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from src.exception import CustomException
 from src.logger import logging
 import sys
 from src.components.data_ingestion import DataIngestion
+from src.components.data_transformation import DataTransformation
 
 if __name__=="__main__":
     try:
         data_ingestion = DataIngestion()
-        data_ingestion.initiate_data_ingestion()
+        train_data_path,test_data_path = data_ingestion.initiate_data_ingestion()
+        data_transformation=DataTransformation()
+        data_transformation.initiate_data_transformation(train_data_path,test_data_path)
         
     except Exception as e:
         logging.error(e)
