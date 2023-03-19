9be47032834517d6b5d4c2c79191137752dfa8e4
gdharani1996 <gdharanidharan07@gmail.com>
2023-03-19 20:36:37 +0530

    main

--- main.py
+++ main.py
@@ -4,19 +4,12 @@ import sys
 from src.components.data_ingestion import DataIngestion
 from src.components.data_transformation import DataTransformation
 from src.components.model_trainer import ModelTrainer
+from src.pipeline.training_pipeline import TrainPipeline
 
 if __name__=="__main__":
     try:
-        print('Training Pipeline Started')
-        logging.info('Training Pipeline Started')
-        data_ingestion = DataIngestion()
-        train_data_path,test_data_path = data_ingestion.initiate_data_ingestion()
-        data_transformation=DataTransformation()
-        train_array,test_array,_ = data_transformation.initiate_data_transformation(train_data_path,test_data_path)
-        model_trainer = ModelTrainer()
-        model_trainer.initiate_model_trainer(train_array,test_array)
-        logging.info('Training Pipeline Completed')
-        print('Training Pipeline Completed')
+        train_pipeline = TrainPipeline()
+        train_pipeline.start_train_pipeline()
         
     except Exception as e:
         logging.error(e)
